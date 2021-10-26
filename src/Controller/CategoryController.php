754df7ca941b08739999e0e33678636e2377df11
NguyenDuong-GCH0804 <84314118+NguyenDuong-GCH0804@users.noreply.github.com>
2021-10-26 22:45:25 +0700

    update

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -37,4 +37,39 @@ class CategoryController extends AbstractController
         $this->addFlash('Warn', 'Deleted category successfully!');
         return $this->redirectToRoute('index_category');  
     }
+    #[Route('/delete/{id}', name: 'delete_category')]
+    public function deleteCategoryAction($id)
+    {
+        $category = $this->getDoctrine()->getRepository(Category::class)->find($id);
+
+        if(!$category){
+            $this->addFlash('Error', 'Category not found!');
+            return $this->redirectToRoute('index_category');           
+        }
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($category);
+        $manager->flush();
+        $this->addFlash('Warn', 'Deleted category successfully!!');
+        return $this->redirectToRoute('index_category');  
+    }
+    #[Route('/add', name: 'add_category')]
+    public function addCategoryAction(Request $request)
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($category);
+            $manager->flush();
+            $this->addFlash('Success', 'Add category successfully!!');
+            return $this->redirectToRoute('index_category');
+        }
+        return $this->render('category/add-edit.html.twig',
+        [
+            'form' => $form->createView(),
+            'edit' => false
+        ]
+    );  
+    }
 }
