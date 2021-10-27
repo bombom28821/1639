5262f1c39faed4a4ea9948f1beb21822adb8f90c
DucMinh <bomaygalc123@gmail.com>
2021-10-27 21:48:24 +0700

    Edit Book

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -98,4 +98,67 @@ class BookController extends AbstractController
             ]
         );
     }
+
+    #[Route('/edit/{id}', name: 'edit_book')]
+    public function editBookAction(Request $request, $id)
+    {
+        $book = $this->getDoctrine()->getRepository(Book::class)->find($id);
+        $orderQuantity = $book->getOrderQuantity();
+        if (!$book) {
+            $this->addFlash('Error', 'Book not found!');
+            return $this->redirectToRoute('index_book');
+        } else {
+            $form = $this->createForm(BookType::class, $book);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                //code xử lý ảnh upload
+                //B1: Lấy dữ liệu từ form
+                $file = $form['cover']->getData();
+                //B2: check xem file ảnh upload có null không
+                if ($file) {
+                    //B3: Lấy dữ liệu ảnh từ file upload
+                    $image = $book->getCover();
+                    //B4: tạo tên mới cho ảnh => tên file ảnh là duy nhất
+                    $imgName = uniqid(); //unique ID
+                    //B5: lấy ra đuôi (extension) của ảnh
+                    $imgExtension = $image->guessExtension();
+                    //B6: gộp tên mới + đuôi tạo thành tên file ảnh hoàn thiện
+                    $imageName = $imgName . "." . $imgExtension;
+                    //B7: di chuyển file ảnh upload vào thư mục chỉ định
+                    try {
+                        $image->move(
+                            $this->getParameter('book_cover'),
+                            $imageName
+                            //Lưu ý: cần khai báo tham số đường dẫn của thư mục cho "book_cover" ở file config/services.yaml
+                        );
+                    } catch (FileException $e) {
+                        //throwException($e);
+                    }
+                    //B8: lưu tên vào database
+                    $book->setCover($imageName);
+                }
+                $quantityForm = $form['quantity']->getData();
+                if($quantityForm < $orderQuantity){
+                    $this->addFlash('Error', 'Quantity invalid!');
+                    return $this->redirectToRoute('edit_book',[
+                        'id' => $id
+                    ]);
+                }
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($book);
+                $manager->flush();
+
+                $this->addFlash('Success', 'Edit book successfully!');
+                return $this->redirectToRoute('index_book');
+            }
+            return $this->render(
+                'book/add-edit.html.twig',
+                [
+                    'form' => $form->createView(),
+                    'edit' => true,
+                    'orderQuantity' => $orderQuantity,
+                ]
+            );
+        }
+    }
 }
